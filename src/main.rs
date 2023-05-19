b74d488ead29cc1842fabd593403702a739a3ccc
MrGunflame <mrgunflame@protonmail.com>
2023-05-20 00:44:54 +0200

    Window setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::{Window, WindowBuilder};
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(800, 600))
+        .build(&event_loop)
+        .unwrap();
+
+    let mut app = unsafe { App::create(&window) };
+    let mut destroying = false;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            // Render a frame if our Vulkan app is not being destroyed.
+            Event::MainEventsCleared if !destroying => unsafe { app.render(&window) },
+            // Destroy our Vulkan app.
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                destroying = true;
+                *control_flow = ControlFlow::Exit;
+
+                unsafe {
+                    app.destroy();
+                }
+            }
+            _ => {}
+        }
+    });
+}
+
+struct App {}
+
+impl App {
+    unsafe fn create(window: &Window) -> Self {
+        Self {}
+    }
+
+    unsafe fn render(&mut self, window: &Window) {}
+
+    unsafe fn destroy(&mut self) {}
 }
